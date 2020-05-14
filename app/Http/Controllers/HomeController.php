7878ea303355a9f7d48f63d8b9b8df3c186129ac
Giang Vu <giangvu@Giangs-MacBook-Pro.local>
2020-05-14 21:55:39 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Booking;
+use App\Feedback;
+use App\Payment;
 use App\User;
+use App\Vehicle;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +15,26 @@ class HomeController extends Controller
     //
     public function index(){
         $User = User::all();
-        return view('admin.home.home', ['User' => $User]);
+
+        $Payment = Payment::whereMonth('created_at', '=', Carbon::now()->month)->get();
+        $PaymentMonthly = 0;
+
+        $Vehicle = Vehicle::all();
+        $Booking = Booking::all();
+        $Feedback = Feedback::orderBy('created_at', 'desc')->take(3)->get();
+
+        foreach ($Payment as $item) {
+            $PaymentMonthly += $item -> total_price;
+        }
+
+
+        return view('admin.home.home', [
+            'User' => $User,
+            'PaymentMonthly' => $PaymentMonthly,
+            'Vehicle' => $Vehicle,
+            'Booking' => $Booking,
+            'Feedback' => $Feedback
+        ]);
     }
 
     public function test(){
